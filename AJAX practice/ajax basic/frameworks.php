499813137a0506311754dfa13843a300ff965ac7
sravanth007 <90679731+sravanth007@users.noreply.github.com>
2022-07-14 18:01:17 +0530

    Add files via upload

---
+++ AJAX practice/ajax basic/frameworks.php
@@ -0,0 +1,33 @@
+<?php
+
+$frameworks = array("Code","Frames","PHP","Oracle") ;
+
+$name = $_GET["name"];
+
+if (strlen($name) > 0) {
+
+    $match = "";
+
+    for ($i = 0; $i < count($frameworks); $i++) {
+
+        if (strtolower($name) == strtolower(substr($frameworks[$i], 0, strlen($name)))) {
+
+            if ($match == "") {
+
+                $match = $frameworks[$i];
+
+            } else {
+
+                $match = $match . " , " . $frameworks[$i];
+
+            }
+
+        }
+
+    }
+
+}
+
+echo ($match == "") ? 'no match found' : $match;
+
+?>
---
+++ AJAX practice/ajax basic/index.php
@@ -0,0 +1,27 @@
+<html>
+
+    <head>
+
+        <title>PHP MVC Frameworks</title>
+
+        <script type="text/javascript" src="auto_complete.js"></script>
+
+    </head>
+
+    <body>
+
+        <h2>Hello World</h2>
+
+        <p><b>Type the first letter of the PHP MVC Framework</b></p>
+
+        <form method="POST" action="index.php">
+
+            <p><input type="text" size="40" id="txtHint"  onkeyup="showName(this.value)"></p>
+
+        </form>
+
+        <p>Matches: <span id="txtName"></span></p>
+
+    </body>
+
+</html>
